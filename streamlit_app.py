4ee49f2ef2bc182b4055f5ff7b1c5dfe52588555
William Lövfors <william.lovfors@liu.se>
2023-09-06 16:39:26 +0200

    Renamed the folder for where the custom package should be installed

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,10 @@ import sys
 import subprocess
 import sys
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--target=./sund", 'https://isbgroup.eu/edu/assets/sund-1.0.1.tar.gz#sha256=669a1d05c5c8b68500086e183d831650277012b3ea57e94356de1987b6e94e3e'])
+if "sund" not in os.listdir('./custom_package'):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--target=./custom_package", 'https://isbgroup.eu/edu/assets/sund-1.0.1.tar.gz#sha256=669a1d05c5c8b68500086e183d831650277012b3ea57e94356de1987b6e94e3e'])
 
-sys.path.append('./sund')
+sys.path.append('./custom_package')
 import sund
 
 # Setup the model and simulation
